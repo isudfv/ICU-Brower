c6bf1dc69ace68b73a33e31e2521b941b249a57a
LotusOvO <LotusOvO@outlook.com>
2022-05-06 15:59:09 +0800

    导航栏操作逻辑

--- src/ICU-Browser-UI/cefwindow.cpp
+++ src/ICU-Browser-UI/cefwindow.cpp
@@ -5,6 +5,7 @@ CEFWindow::CEFWindow()
 {
     //	setStyleSheet("QMainWindow {background: 'yellow';}");
     setFlag(Qt::FramelessWindowHint);
+    browser_url_ = "bilibili.com";
 }
 
 void CEFWindow::resizeCEFWindow(int x, int y, int width, int height)
@@ -14,3 +15,37 @@ void CEFWindow::resizeCEFWindow(int x, int y, int width, int height)
     qDebug() << this->size();
     qDebug() << this->position();
 }
+
+void CEFWindow::doLoadUrl(const QString &url){
+    qDebug() << "Load:" << url;
+}
+
+void CEFWindow::doReload(){
+    qDebug() << "ReLoad";
+}
+
+void CEFWindow::doStopLoad(){
+    qDebug() << "StopLoad";
+}
+
+void CEFWindow::doGoBack(){
+    qDebug() << "GoBack";
+}
+
+void CEFWindow::doGoForward(){
+    qDebug() << "GoForward";
+}
+
+bool CEFWindow::getBackState(){
+    return can_go_back_;
+}
+
+bool CEFWindow::getForwardState(){
+    return can_go_forward_;
+}
+
+QString CEFWindow::getUrl(){
+    return browser_url_;
+}
+
+
--- src/ICU-Browser-UI/cefwindow.h
+++ src/ICU-Browser-UI/cefwindow.h
@@ -5,19 +5,42 @@
 #include <QWidget>
 #include <QWindow>
 
-#include "../qbrowserclient/qbrowserwindow.h"
+//#include "../qbrowserclient/qbrowserwindow.h"
 
 //namespace Ui {
 //    class Form;
 //}
 
-class CEFWindow : public QBrowserWindow {
+class CEFWindow : public QWindow {
  Q_OBJECT
  public:
   CEFWindow();
-  explicit CEFWindow(const CefString &url) : QBrowserWindow(url) {}
+//  explicit CEFWindow(const CefString &url) : QBrowserWindow(url) {}
  public slots:
   void resizeCEFWindow(int x, int y, int width, int height);
+
+  void doLoadUrl(const QString &url);
+
+  void doReload();
+
+  void doStopLoad();
+
+  void doGoBack();
+
+  void doGoForward();
+
+  bool getBackState();
+
+  bool getForwardState();
+
+  QString getUrl();
+private:
+   int browser_id_;
+   QString browser_url_;
+   bool is_closing_ = false;
+   bool is_loading_ = false;
+   bool can_go_back_ = true;
+   bool can_go_forward_ = true;
 };
 
 #endif// CEFWINDOW_H
