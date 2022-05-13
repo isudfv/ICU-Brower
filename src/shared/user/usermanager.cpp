1a56638c6a4e094e5ec46c9ae2a565a018843e69
Wesson <wessonx@163.com>
2022-05-13 22:51:32 +0800

    register surface

---
+++ src/shared/user/usermanager.cpp
@@ -0,0 +1,74 @@
+﻿#include "usermanager.h"
+
+
+Q_INVOKABLE void UserManager::doLogin(QString username,QString password,QJSValue callback)
+{
+    qDebug()<<"123"<<Qt::endl;
+}
+Q_INVOKABLE void UserManager::doRegister(QString username,QString password,QJSValue callback)
+{
+    QJSValue _state;
+    int pwdLen=password.length();
+    int nameLen=username.length();
+    //先判断名字是否合法
+    if(nameLen<8||nameLen>16)
+    {
+        rstate=userNameLengthViolation;
+    }
+    else
+    {
+        //判断密码长度是否合法
+        if(pwdLen<8||pwdLen>16)
+        {
+            rstate=pwdLengthViolation;
+        }
+        else
+        {
+            //建立数据库实例
+            Instance inst={};
+            //标明数据库地址
+            Uri uri("mongodb://localhost:27017");
+            //建立数据库连接
+            Client client(uri);
+
+            //访问相关的表
+            Database db=client["browser"];
+            Collection coll=db["user"];
+
+            //创建过滤器
+            DocumentBuilder doc_builder;
+            doc_builder.append(kvp("username",username.toStdString()));
+            doc_builder.append(kvp("password",password.toStdString()));
+            DocumentValue value=doc_builder.extract();
+            DocumentView view=value.view();
+
+            //在表中查找过滤
+            auto result=coll.find_one(view);
+            //如果找到了重复的,则报错
+            if(result)
+            {
+                rstate=userAlreadyExist;
+            }
+            else
+            {
+                //向表中插入
+                auto result2=coll.insert_one(view);
+                if(result2)
+                    rstate=registerSuccess;
+                else rstate=registerFailure;
+            }
+        }
+    }
+    //调用回调函数，给view层显示状态
+    _state=rstate;
+    QJSValueList list;
+    list.append(_state);
+    callback.call(list);
+
+    qDebug()<<rstate;
+    return ;
+}
+Q_INVOKABLE void UserManager::doLogout(QJSValue callback)
+{
+
+}
---
+++ src/shared/user/usermanager.h
@@ -0,0 +1,57 @@
+﻿#ifndef USERMANAGER_H
+#define USERMANAGER_H
+
+#include <QCoreApplication>
+#include <QJSValue>
+#include<QDebug>
+#include <QString>
+
+#include <mongocxx/client.hpp>
+#include <mongocxx/stdx.hpp>
+#include <mongocxx/uri.hpp>
+#include <mongocxx/instance.hpp>
+#include <bsoncxx/json.hpp>
+#include <bsoncxx/builder/stream/helpers.hpp>
+#include <bsoncxx/builder/stream/document.hpp>
+#include <bsoncxx/builder/stream/array.hpp>
+#include<bsoncxx/builder/basic/document.hpp>
+#include<bsoncxx/builder/basic/impl.hpp>
+#include<bsoncxx/string/to_string.hpp>
+#include<bsoncxx/stdx/string_view.hpp>
+#include <bsoncxx/types.hpp>
+#include <bsoncxx/types/bson_value/value.hpp>
+using bsoncxx::builder::basic::kvp;
+using Qt::endl;
+using bsoncxx::to_json;
+
+typedef mongocxx::instance Instance;
+typedef mongocxx::client Client;
+typedef mongocxx::uri Uri;
+typedef mongocxx::database Database;
+typedef mongocxx::collection Collection;
+typedef bsoncxx::builder::basic::document DocumentBuilder;
+typedef bsoncxx::document::value DocumentValue;
+typedef bsoncxx::document::view DocumentView;
+
+
+class UserManager:public QObject{
+    Q_OBJECT
+public:
+    Q_INVOKABLE void doLogin(QString username,QString password,QJSValue callback);
+    Q_INVOKABLE void doRegister(QString username,QString password,QJSValue callback);
+    Q_INVOKABLE void doLogout(QJSValue callback);
+    enum registerstate{
+        registerSuccess,
+        registerFailure,
+        userAlreadyExist,
+        userNameLengthViolation,
+        pwdLengthViolation
+    }rstate;
+    enum loginstate{
+        loginSuccess,
+        userNotExist,
+        passwordError
+    }lstate;
+};
+
+#endif // USERMANAGER_H
