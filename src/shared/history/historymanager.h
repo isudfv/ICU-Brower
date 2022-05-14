c298c33badc639de51538d072f933a468e1bf649
Wesson <wessonx@163.com>
2022-05-14 22:23:55 +0800

    add and modify some arguments, plus one function

--- src/shared/history/historymanager.h
+++ src/shared/history/historymanager.h
@@ -11,19 +11,16 @@
 #include<QJsonArray>
 #include<QJSValue>
 #include <QDir>
+#include <QDateTime>
 using Qt::endl;
 class HistoryManager:public QObject{
     Q_OBJECT
 public:
-    static HistoryManager* getInstanse(){
-        static HistoryManager fm;
-        return &fm;
-    }
-
-    Q_INVOKABLE void addHistory(QString name,QString time,QString url,int uid,QJSValue callback);
-    Q_INVOKABLE void removeHistory(QString name,QString time,QString url,int uid,QJSValue callback);
+    Q_INVOKABLE void addHistory(QString name,QString url,int uid,QJSValue callback);
+    Q_INVOKABLE void removeHistory(QString name,QString url,QString date,QString time,int uid,QJSValue callback);
     Q_INVOKABLE void clearHistory(int uid,QJSValue callback);
-    Q_INVOKABLE void loadHistory(int uid,QJSValue callbacka,QJSValue callbackb);
+    Q_INVOKABLE void LoadHistory(int uid,QJSValue callbacka,QJSValue callbackb);
+    Q_INVOKABLE void removeSignalDayHistory(int uid,QString date,QJSValue callback);
 
 };
 
