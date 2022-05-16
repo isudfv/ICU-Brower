02d8c3c37c1f2e6b0e5577dd24b582b511996f81
Wesson <wessonx@163.com>
2022-05-16 11:41:15 +0800

    add synchronize interface for "favourite"

---
+++ src/shared/synchronize/synchronize.h
@@ -0,0 +1,4 @@
+#ifndef SYNCHRONIZE_H
+#define SYNCHRONIZE_H
+
+#endif // SYNCHRONIZE_H
--- src/shared/synchronize/syncmanager.cpp
+++ src/shared/synchronize/syncmanager.cpp
@@ -17,11 +17,13 @@ DocumentValue toBson(QJsonObject&obj)
 }
 QJsonObject toJson(DocumentValue&value)
 {
+    //为了保证写回本地之后，仍然是intented的格式，有必要转换成string之后，再转换成document，再写回文件；
+    //而不是直接将string写回file
     if(value.view().empty())
         return QJsonObject();
     std::string json=bsoncxx::to_json(value);
 
-    qDebug()<<QString::fromStdString(json);
+//    qDebug()<<QString::fromStdString(json);
     QByteArray array=json.data();
     QJsonDocument doc=QJsonDocument::fromJson(array);
     QJsonObject obj=doc.object();
@@ -44,7 +46,7 @@ void syncManager::syncHistoryFromLocal(int uid)
 
     file.close();
 
-    qDebug()<<QString::fromStdString(bsoncxx::to_json(value));
+//    qDebug()<<QString::fromStdString(bsoncxx::to_json(value));
 
     //和数据库建立连接
     Instance inst{};
@@ -118,4 +120,79 @@ void syncManager::syncHistoryToLocal(int uid)
 
 
 }
+void syncManager::syncFavouriteFromLocal(int uid)
+{
+    //打开文件
+    QString file_path=QString("./favourite/%1.json").arg(uid);
+    QFile file(file_path);
+    file.open(QIODevice::ReadOnly);
 
+    //获取json文件对象
+    QJsonParseError error;
+    QJsonDocument doc=QJsonDocument::fromJson(file.readAll(),&error);
+    QJsonObject obj=doc.object();
+
+    DocumentValue value=toBson(obj);
+
+//    qDebug()<<QString::fromStdString(bsoncxx::to_json(value));
+
+    file.close();
+    //与数据库建立连接
+    Instance inst{};
+    Uri uri("mongodb://175.178.155.66:27017");
+    Client client(uri);
+
+    Database db=client["browser"];
+    Collection coll=db["favourite"];
+
+    //判断用户是否存在
+    auto result=coll.find_one(DocumentStreamBuilder{}<<"_id"<<uid<<finalize);
+    if(!result)
+        coll.insert_one(value.view());
+    else
+    {
+        coll.find_one_and_update(DocumentStreamBuilder{}<<"_id"<<uid<<finalize,value.view());
+    }
+
+
+
+}
+void syncManager::syncFavouriteToLocal(int uid)
+{
+    //和数据库建立连接
+    Instance inst={};
+    Uri uri("mongodb://175.178.155.66:27017");
+    Client client(uri);
+
+    Database db=client["browser"];
+    Collection coll=db["favourite"];
+
+    //判断是否存在该用户
+    auto result=coll.find_one(DocumentStreamBuilder{}<<"_id"<<uid<<finalize);
+
+    if(!result)
+        return;
+
+    //将bson转换成json格式
+    DocumentValue value=result.value();
+//    qDebug()<<QString::fromStdString(bsoncxx::to_json(value));
+
+    QJsonObject obj=toJson(value);
+
+    QJsonDocument doc(obj);
+
+    //打开本地文件，并写回
+    QString file_path=QString("./favourite/%1.json").arg(uid);
+    QFile file(file_path);
+    file.open(QIODevice::WriteOnly|QIODevice::Truncate);
+
+    QByteArray data=doc.toJson(QJsonDocument::Indented);
+
+    file.write(data);
+
+    file.close();
+
+
+
+
+}
